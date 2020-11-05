130beca792d390aaf2f15484aec46e0a6ac0cbed
ADF <adifferentfur.up@gmail.com>
2020-11-05 03:57:28 -0800

    removing the local state

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,28 +1,40 @@
-import React, { Component } from 'react';
-import Counter from './counter';
+import React, { Component } from "react";
+import Counter from "./counter";
 
 class Counters extends Component {
-    state = {
-        counters: [
-            { id: 1, value: 0 },
-            { id: 2, value: 1 },
-            { id: 3, value: 2 },
-            { id: 4, value: 3 },
-        ]
-    };
-    handleDelete = (counterId) => {
-        const counters = this.state.counters.filter(c => c.id !== counterId);
-        this.setState({ counters });
-    }
-    render() {
-        return (
-            <div>
-                {this.state.counters.map(counter => (
-                    <Counter key={counter.id} onDelete={this.handleDelete} counter={counter} />
-                ))}
-            </div>
-        );
-    }
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 1 },
+      { id: 3, value: 2 },
+      { id: 4, value: 3 },
+    ],
+  };
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = this.state.counters.indexOf(counter);
+    counters[index] = [...counter];
+    counters[index].value++;
+    this.setState(counters);
+  };
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters });
+  };
+  render() {
+    return (
+      <div>
+        {this.state.counters.map((counter) => (
+          <Counter
+            key={counter.id}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+            counter={counter}
+          />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default Counters;
