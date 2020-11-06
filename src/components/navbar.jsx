565e69aa829a0d54c6145a58002898cb9cc1be3c
ADF <adifferentfur.up@gmail.com>
2020-11-05 23:05:04 -0800

    lifting state up

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,7 +6,9 @@ class NavBar extends Component {
     return (
       <nav class="navbar navbar-light bg-light">
         <a class="navbar-brand" href="#">
-          Navbar
+          <span className="badge badge-pill badge-secondary">
+            {this.props.totalCounters}
+          </span>
         </a>
       </nav>
     );
